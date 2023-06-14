05edd5ee805856a7b953ddd5047fbcadc70a9562
Nguyễn Bá Đại <it.dainb@gmail.com>
2023-06-15 00:22:27 +0700

    Update tree folder

---
+++ Round_4/plugin.js
@@ -0,0 +1,42 @@
+window.onload = function() {
+    // Access the carMarker variable from the global object
+    carMarker = window.carMarker;
+    chooseRoad = window.chooseRoad;
+
+};
+
+
+const map = ({ widgets, simulator, vehicle }) => {
+    const map_div = document.createElement("div");
+    
+    map_div.innerHTML = `
+    <title>Car Drawing and Moving Map</title>
+    <style>
+        #map {
+            position: relative;
+            width: 500px;
+            height: 300px;
+            overflow: hidden;
+            border: 1px solid black;
+        }
+
+        #carCanvas {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+    </style>
+    <link rel="stylesheet" href="leaflet/leaflet.css"/>
+    <div id="map"></div>
+    <script src="leaflet/leaflet.js"></script>
+    <script src="leaflet/leaflet.rotatedMarker.js"></script>
+    <script src="script.js"></script>
+    <script src="plugin.js"></script>
+    `;
+
+    // Append the div to the document body
+    // document.body.appendChild(map_div);
+};
+
+export default map;
