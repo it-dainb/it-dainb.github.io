cdd602b0496676b5016cbc6f22c1f30977707e4e
Nguyễn Bá Đại <it.dainb@gmail.com>
2023-06-15 09:35:03 +0700

    Update plugin.js

--- Round_4/plugin.js
+++ Round_4/plugin.js
@@ -1,22 +1,47 @@
-const plugin = ({ widgets, simulator, vehicle }) => {
-    const sampleDiv = document.createElement("div")
-    sampleDiv.innerHTML = `
-    <div style='font-size:20px;width:100%;height:100%;background-color:yellow;color:red;text-align:center;margin:auto;'>
-        <div> Wiper status:  <span id="wiper">Unknown</span></div>
-    </div>
-    `
+// window.onload = function() {
+//     // Access the carMarker variable from the global object
+//     carMarker = window.carMarker;
+//     chooseRoad = window.chooseRoad;
 
-    let wiper = sampleDiv.querySelector("#wiper")
+// };
 
-    setInterval(async () => {
-        let value = await vehicle['Body.Windshield.Front.Wiping.Mode'].get()
-        wiper.innerHTML = value
-    }, 500)
 
-    widgets.register("map", 
-    (box) => {
-        box.injectNode(sampleDiv)
-    })
-}
+const map = ({ widgets, simulator, vehicle }) => {
+    const map_div = document.createElement("div");
+    
+    map_div.innerHTML = `
+    <title>Car Drawing and Moving Map</title>
+    <style>
+        #map {
+            position: relative;
+            width: 500px;
+            height: 300px;
+            overflow: hidden;
+            border: 1px solid black;
+        }
 
-export default plugin;
+        #carCanvas {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+    </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/it-dainb/it-dainb.github.io@main/Round_4/leaflet/leaflet.css"/>
+    <div id="map"></div>
+    <script src="https://cdn.jsdelivr.net/gh/it-dainb/it-dainb.github.io@main/Round_4/leaflet/leaflet.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/it-dainb/it-dainb.github.io@main/Round_4/leaflet/leaflet.rotatedMarker.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/it-dainb/it-dainb.github.io@main/Round_4/script.js"></script>
+    <!-- <script src="plugin.js"></script> -->
+    `;
+
+    // Append the div to the document body
+    // document.body.appendChild(map_div);
+
+    widgets.register("map", (box) => {
+        box.injectNode(map_div);
+    });
+
+};
+
+export default map;
