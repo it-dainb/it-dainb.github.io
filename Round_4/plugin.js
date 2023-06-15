12b15a6bd8cbd58cdfe15998b4734b41a3987801
Nguyễn Bá Đại <it.dainb@gmail.com>
2023-06-15 21:54:59 +0700

    Update car

--- Round_4/plugin.js
+++ Round_4/plugin.js
@@ -1,21 +1,68 @@
-// window.onload = function() {
-//     // Access the carMarker variable from the global object
-//     carMarker = window.carMarker;
-//     chooseRoad = window.chooseRoad;
+function findHeading(angle) {
+    let heading;
+    
+    if (angle % 360 == 0) {
+        heading = "east";
+    } else if (angle % 90 == 0) {
+        heading = "south";
+    } else if (angle % 180 == 0) {
+        heading = "west";
+    } else {
+        heading = "north";
+    }
 
-// };
+    return heading;
+}
 
+class Car {
+    constructor(element, L, zone) {
+        this.element = element;
+        this.speed = 0;
+        this.angle = element.options.rotationAngle;
+        
+        this.heading = findHeading(this.angle
+            );
+        this.pos = this.element.getLatLng();
+        this.L = L;
+        this.zone = zone;
+    }
+
+    move(speed) {
+        this.speed = speed;
+    }
+
+    stop() {
+        this.speed = 0;
+    }
+
+    rotate(angle) {
+        this.angle += angle;
+        this.element.setRotationAngle(this.angle);
+
+        this.heading = findHeading(this.angle);
+    }
+
+    update(map) {
+        let pos = this.element.getLatLng();
+        let speed = this.speed / 1000 * 10;
+        let angle = this.angle * Math.PI / 180;
+
+        pos = this.L.latLng(pos.lat + -1 * Math.sin(angle) * speed, pos.lng + Math.cos(angle) * speed);
+        this.element.setLatLng(pos);
+
+        map.panTo(pos);
+    }
+}
+
+var car;
 
 const map = ({ widgets, simulator, vehicle }) => {
-    const map_div = document.createElement("div");
-    
-
-    var head = `
+    var iframe_head = `
     <style>
         #map {
             position: relative;
-            width: 500px;
-            height: 300px;
+            width: 733px;
+            height: 733px;
             overflow: hidden;
             border: 1px solid black;
         }
@@ -32,28 +79,78 @@ const map = ({ widgets, simulator, vehicle }) => {
     <script src="https://it-dainb.github.io/Round_4/leaflet/leaflet.rotatedMarker.js"></script>
     `;
     
-    var body = `
+    var iframe_body = `
     <div id="map"></div>
-    <script src="https://it-dainb.github.io/Round_4/script.js"></script>
+    <script src="http://localhost:5500/Round_4/script.js"></script>
     <!-- <script src="plugin.js"></script> -->
     `;
 
-    map_div.innerHTML = `
-        
-    `;
 
     // Append the div to the document body
     // document.body.appendChild(map_div);
 
+    var iframeContent = '<!DOCTYPE html>' +
+            '<html>' +
+            '<head>' +
+            iframe_head +
+            '</head>' +
+            '<body>' +
+            iframe_body +
+            '</body>' +
+            '</html>';
+    
+    var oneTime = true;
+    
     widgets.register("map", (box) => {
-        let box_document = box.window["frameElement"].contentDocument;
-        let box_head = box_document.head;
-        let box_body = box_document.body;
+        let box_window = box.window;
+        let box_iframe = box_window.frameElement;
+        let box_document = box_iframe.contentDocument;
+        
+        box_iframe.srcdoc = iframeContent;
 
-        box_head.innerHTML = head;
-        box_body.innerHTML = body;
-        // box.document.head.innerHTML = head;
-        // box.injectNode(map_div);
+        box_iframe.addEventListener("load", () => {
+            let iframeWindow = box_iframe.contentWindow;
+            
+            let carMarker = iframeWindow.carMarker;
+            let mapContainer = iframeWindow.document.getElementById("map");
+            let map = iframeWindow.map;
+            
+            // map.setZoom(1.5);
+            car = new Car(carMarker, iframeWindow.L, box_window.circle);
+
+            box_window.changeColor('y');
+
+            // car.rotate(90);
+            car.move(60);
+            // car.update();
+
+            // Adjust map container dimensions
+            function resizeMap() {
+                const width = box_iframe.clientWidth;
+                const height = box_iframe.clientHeight;
+                mapContainer.style.width = width + "px";
+                mapContainer.style.height = height + "px";
+            }
+
+            // resizeMap();
+            
+            box_iframe.contentWindow.addEventListener("resize", resizeMap);
+            // Access the carMarker variable here
+            setInterval(() => {
+                // console.log(box_window.canTurn);
+
+                if (box_window.canTurn && oneTime) {
+                    car.rotate(90);
+                    oneTime = false;
+                } else if (!box_window.canTurn) {
+                    oneTime = true;
+                }
+
+
+
+                car.update(map);
+            }, 10)
+        });
     });
 
 };
--- Round_4/script.js
+++ Round_4/script.js
@@ -1,156 +1,262 @@
+function calculateDistance(x1, y1, x2, y2) {
+    // Calculate the differences between the coordinates
+    var deltaX = x2 - x1;
+    var deltaY = y2 - y1;
 
-// Create the map
-const map = L.map('map', {
-    crs: L.CRS.Simple,
-    minZoom: 0,
-    maxZoom: 0,
-    zoomSnap: 0.5,
-    zoomDelta: 0.5,
-    zoomControl: false,
-}).setView([0, 0], 0);
+    // Calculate the squared distances
+    var squaredDeltaX = deltaX * deltaX;
+    var squaredDeltaY = deltaY * deltaY;
 
-// Define the image bounds and dimensions
-const bounds = [[0, 0], [500, 500]];
-const imageOptions = { opacity: 1, attribution: 'Crossroad Image' };
+    // Calculate the squared distance between the two points
+    var squaredDistance = squaredDeltaX + squaredDeltaY;
 
-var root_path = "https://it-dainb.github.io/Round_4/images/"
-const imageURL = root_path + 'crossroad.png';
-const iconURL = root_path + 'car.png';
+    // Calculate the absolute distance
+    var distance = Math.sqrt(squaredDistance);
 
-// Create the custom image overlay representing the crossroad
-const imageOverlay = L.imageOverlay(imageURL, bounds, imageOptions).addTo(map)
-var imageWidth = imageOverlay._image.width;
-var imageHeight = imageOverlay._image.height;
+    // console.log(deltaX);
+    // console.log(deltaY);
 
-// Fit the map to the image bounds
-map.fitBounds(imageOverlay.getBounds());
+    return distance;
+}
 
-// Draw the car marker
-// ambulance: 90: 70
+;window.addEventListener('load', () => {
+    // Create the map
+    window.L = L;
 
-var iconSize = 70;
-const carIcon = L.icon({
-    iconUrl: iconURL, // Replace with the URL to your car icon image
-    iconSize: [iconSize, iconSize],
-    iconAnchor: [iconSize / 2, iconSize / 2],
+    const map = L.map('map', {
+        crs: L.CRS.Simple,
+        minZoom: 0,
+        maxZoom: 2,
+        zoomSnap: 0.5,
+        zoomDelta: 0.5,
+        zoomControl: false,
+        dragging: false,
+    }).setView([0, 0], 0);
+
+    window.map = map;
+    var scale = 1;
+    map.setZoom(scale);
+
+    // Define the image bounds and dimensions
+    const bounds = [[0, 0], [500, 500]];
+    const imageOptions = { opacity: 1, attribution: 'Crossroad Image' };
+    
+    const images_path = "https://it-dainb.github.io/Round_4/images/"
+    
+    const imageURL = images_path + 'crossroad.png';
+    const iconUrl = images_path + 'car.png';
+
+    // Create the custom image overlay representing the crossroad
+    const imageOverlay = L.imageOverlay(imageURL, bounds, imageOptions).addTo(map)
+    var imageWidth = imageOverlay._image.width;
+    var imageHeight = imageOverlay._image.height;
+    
+    // Fit the map to the image bounds
+    map.fitBounds(imageOverlay.getBounds());
+    
+    // Draw the car marker
+    // ambulance: 90: 70
+    
+    var iconSize = 90  * scale;
+    const carIcon = L.icon({
+        iconUrl: iconUrl, // Replace with the URL to your car icon image
+        iconSize: [iconSize, iconSize],
+        iconAnchor: [iconSize / 2, iconSize / 2],
+    });
+    
+    var center = imageOverlay.getCenter();
+    var carMarker = L.marker(
+        [center.lat - 25, center.lng],
+        {   icon: carIcon ,
+            rotationAngle: 0,
+            rotationOrigin: "center",
+        }
+    ).addTo(map);
+    window.carMarker = carMarker;
+    
+    var green = '#c0ff33';
+    var red = '#f3252e';
+    var yellow = '#fbff32'
+
+    var root_radius = 70
+    var circle = L.circle(
+        carMarker.getLatLng(), {
+        color: 'red',
+        fillColor: red,
+        fillOpacity: 0.4,
+        radius: 70,
+        weight: 1,
+    }).addTo(map);
+
+    function changeColor(color) {
+
+        if (color === "r") {
+            circle.setStyle({
+                color: 'red',
+                fillColor: red,
+            })
+        } else if (color === "y") {
+            circle.setStyle({
+                color: 'yellow',
+                fillColor: yellow,
+            })
+        } else {
+            circle.setStyle({
+                color: 'green',
+                fillColor: green,
+            })
+        }
+
+    }
+
+    window.changeColor = changeColor;
+    window.circle = circle;
+
+        
+    function createUD(imageOverlay) {
+        let imgBounds = imageOverlay.getBounds();
+        
+        let southWest = imgBounds.getSouthWest();
+        let northEast = imgBounds.getNorthEast();
+
+        let upImage = L.imageOverlay(imageURL, L.latLngBounds([northEast.lat - 4, southWest.lng], [northEast.lat + imageHeight, northEast.lng]), imageOptions).addTo(map);
+        let downImage = L.imageOverlay(imageURL, L.latLngBounds([southWest.lat  - imageHeight, southWest.lng], [southWest.lat + 4, northEast.lng]), imageOptions).addTo(map);
+        return [upImage, downImage];
+    };
+
+    function createLR(imageOverlay) {
+        let imgBounds = imageOverlay.getBounds();
+        let southWest = imgBounds.getSouthWest();
+        let northEast = imgBounds.getNorthEast();
+
+        let leftImage = L.imageOverlay(imageURL, L.latLngBounds([southWest.lat, southWest.lng - imageWidth], [northEast.lat, southWest.lng + 4]), imageOptions).addTo(map);
+        let rightImage = L.imageOverlay(imageURL, L.latLngBounds([southWest.lat, northEast.lng - 4], [northEast.lat, northEast.lng + imageWidth]), imageOptions).addTo(map);
+        return [leftImage, rightImage];
+    }
+    
+    // createUD(imageOverlay);
+
+    function createOV(imageOverlay) {
+        let [U, D] = createUD(imageOverlay);
+        let [cL, R] = createLR(imageOverlay);
+        let [RU, RD] = createUD(R);
+        let [LU, LR] = createUD(cL);
+
+        return [imageOverlay, U, D, cL, R, RU, RD, LU, LR];
+    }
+
+    // createOV(imageOverlay);
+     // Duplicate the crossroad image in four directions
+    //  const leftImage = L.imageOverlay(imageURL, L.latLngBounds([imageHeight - 2, 0], [2 * imageHeight, imageWidth]), imageOptions).addTo(map);
+    //  const leftImage = L.imageOverlay(imageURL, L.latLngBounds([imageHeight - 2, 0], [2 * imageHeight, imageWidth]), imageOptions).addTo(map);
+
+    var createMapOV = true;
+    var chooseRoad = imageOverlay;
+    
+    let carPosition = carMarker.getLatLng();
+    center = chooseRoad.getCenter();
+
+    window.chooseRoad = chooseRoad;
+
+    // createOV(imageOverlay);
+    var cR, cL, cU, cD;
+    var C, U, D, cL, R, RU, RD, LU, LR;
+
+    var carX, carY;
+
+    var canTurn;
+
+    console.log(circle);
+
+    var radius = 1;
+    var radius_speed = root_radius / 6 / 1000 * 10;
+    var radius_min = 60;
+    setInterval(() => {
+        
+        carPosition = carMarker.getLatLng();
+        carX = carPosition.lng;
+        carY = carPosition.lat;
+
+        circle.setLatLng(carPosition);
+        
+        if (radius > root_radius) {
+            radius_speed *= -1;
+            radius = root_radius;
+        } else if (radius < radius_min) {
+            radius_speed *= -1;
+            radius = radius_min;
+        }
+        
+        radius += radius_speed;
+
+        circle.setRadius(radius)
+
+        // map.panTo(carPosition);
+        
+        // console.log(carX);
+        if (createMapOV === true) {
+
+            [C, U, D, cL, R, RU, RD, LU, LR] = createOV(chooseRoad);
+            let cBounds = C.getBounds();
+            let cSW = cBounds.getSouthWest();
+            let cNE = cBounds.getNorthEast();
+            
+            cR = cNE.lng - imageWidth / 4;
+            ccL = cSW.lng + imageWidth / 4; 
+            cU = cNE.lat - imageHeight / 4;
+            cD = cSW.lat + imageHeight / 4;
+        
+            console.log("CR", cR);
+            console.log("CL", cL);
+            console.log("CU", cU);
+            console.log("CD", cD);
+
+            createMapOV = false;
+            center = chooseRoad.getCenter();
+        }
+
+        // console.log(center.lat);
+        // console.log(carY);
+        let distance = calculateDistance(center.lng, center.lat, carX, carY);
+        
+        canTurn = false;
+        // let distance = center.distanceTo(carPosition);
+        if (36 < distance && distance < 40) {
+            // console.log(distance);
+            canTurn = true;
+            // console.log(canTurn);
+            changeColor("g");
+        } else {
+            changeColor("y");
+        }
+
+        window.canTurn = canTurn;
+        
+        if (!createMapOV && (!chooseRoad.getBounds().contains(carMarker.getLatLng()))) {
+            if (carX > cR) {
+                console.log("ADD RIGHT");
+                chooseRoad = R;
+                createMapOV = true;
+            } else if (carX < ccL) {
+                console.log("ADD LEFT");
+                chooseRoad = cL;
+                createMapOV = true;
+            }
+            if (carY > cU) {
+                console.log("ADD UP");
+                chooseRoad = U;
+                createMapOV = true;
+            } else if (carY < cD) {
+                console.log("ADD DOWN");
+                chooseRoad = D;
+                createMapOV = true;
+            }
+            
+        }
+        // console.log(createMapOV);
+        
+    }, 10);
+
 });
-
-var carMarker = L.marker(
-    [250 - iconSize / 2 + 10, 250],
-    {   icon: carIcon ,
-        rotationAngle: 0,
-        rotationOrigin: "center"
-    }
-).addTo(map);
-
-window.carMarker = carMarker;
-    
-function createUD(imageOverlay) {
-    let imgBounds = imageOverlay.getBounds();
-    
-    let southWest = imgBounds.getSouthWest();
-    let northEast = imgBounds.getNorthEast();
-
-    let upImage = L.imageOverlay(imageURL, L.latLngBounds([northEast.lat - 4, southWest.lng], [northEast.lat + imageHeight, northEast.lng]), imageOptions).addTo(map);
-    let downImage = L.imageOverlay(imageURL, L.latLngBounds([southWest.lat  - imageHeight, southWest.lng], [southWest.lat + 4, northEast.lng]), imageOptions).addTo(map);
-    return [upImage, downImage];
-};
-
-function createLR(imageOverlay) {
-    let imgBounds = imageOverlay.getBounds();
-    let southWest = imgBounds.getSouthWest();
-    let northEast = imgBounds.getNorthEast();
-
-    let leftImage = L.imageOverlay(imageURL, L.latLngBounds([southWest.lat, southWest.lng - imageWidth], [northEast.lat, southWest.lng + 4]), imageOptions).addTo(map);
-    let rightImage = L.imageOverlay(imageURL, L.latLngBounds([southWest.lat, northEast.lng - 4], [northEast.lat, northEast.lng + imageWidth]), imageOptions).addTo(map);
-    return [leftImage, rightImage];
-}
-
-// createUD(imageOverlay);
-
-function createOV(imageOverlay) {
-    let [U, D] = createUD(imageOverlay);
-    let [cL, R] = createLR(imageOverlay);
-    let [RU, RD] = createUD(R);
-    let [LU, LR] = createUD(cL);
-
-    return [imageOverlay, U, D, cL, R, RU, RD, LU, LR];
-}
-
-// createOV(imageOverlay);
-    // Duplicate the crossroad image in four directions
-//  const leftImage = L.imageOverlay(imageURL, L.latLngBounds([imageHeight - 2, 0], [2 * imageHeight, imageWidth]), imageOptions).addTo(map);
-//  const leftImage = L.imageOverlay(imageURL, L.latLngBounds([imageHeight - 2, 0], [2 * imageHeight, imageWidth]), imageOptions).addTo(map);
-
-// Move the car marker
-const tick = 10; //ms
-const speed = 80; //px/s
-let carPosition = carMarker.getLatLng();
-
-var createMapOV = true;
-var chooseRoad = imageOverlay;
-
-window.chooseRoad = chooseRoad;
-
-// createOV(imageOverlay);
-var cR, cL, cU, cD;
-var C, U, D, cL, R, RU, RD, LU, LR;
-
-setInterval(() => {
-    
-    
-    var carX = carPosition.lng;
-    var cary = carPosition.lat;
-    
-    map.panTo(carPosition);
-    carMarker.setLatLng(carPosition);
-    
-    if (createMapOV === true) {
-
-        [C, U, D, cL, R, RU, RD, LU, LR] = createOV(chooseRoad);
-        let cBounds = C.getBounds();
-        let cSW = cBounds.getSouthWest();
-        let cNE = cBounds.getNorthEast();
-        
-        cR = cNE.lng - imageWidth / 4;
-        cL = cSW.lng + imageWidth / 4; 
-        cU = cNE.lat - imageHeight / 4;
-        cD = cSW.lat + imageHeight / 4;
-    
-        // console.log("CR", cR);
-        // console.log("CL", cL);
-        // console.log("CU", cU);
-        // console.log("CD", cD);
-
-        createMapOV = false;
-    }
-    
-
-
-    // console.log(carX);
-    // console.log(cR);
-    // console.log("=====");
-
-    if (!createMapOV && (!chooseRoad.getBounds().contains(carMarker.getLatLng()))) {
-        if (carX > cR) {
-            console.log("ADD RIGHT");
-            chooseRoad = R;
-        } else if (carX < cL) {
-            console.log("ADD LEFT");
-            chooseRoad = L;
-        }
-        if (cary > cU) {
-            console.log("ADD UP");
-            chooseRoad = U;
-        } else if (cary < cD) {
-            console.log("ADD DOWN");
-            chooseRoad = D;
-        }
-        createMapOV = true;
-        
-    }
-    
-}, 10);
-
 
 
