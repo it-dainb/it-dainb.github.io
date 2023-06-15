1783853468fbc534fd1f453aa6da2eb7081483f0
Nguyễn Bá Đại <it.dainb@gmail.com>
2023-06-15 08:56:29 +0700

    update online link

--- Round_4/plugin.js
+++ Round_4/plugin.js
@@ -10,8 +10,9 @@ const map = ({ widgets, simulator, vehicle }) => {
     const map_div = document.createElement("div");
     
     map_div.innerHTML = `
+    <title>Car Drawing and Moving Map</title>
     <style>
-        .map {
+        #map {
             position: relative;
             width: 500px;
             height: 300px;
@@ -19,13 +20,19 @@ const map = ({ widgets, simulator, vehicle }) => {
             border: 1px solid black;
         }
 
-        .carCanvas {
+        #carCanvas {
             position: absolute;
             top: 50%;
             left: 50%;
             transform: translate(-50%, -50%);
         }
     </style>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/it-dainb/it-dainb.github.io@main/Round_4/leaflet/leaflet.css"/>
+    <div id="map"></div>
+    <script src="https://cdn.jsdelivr.net/gh/it-dainb/it-dainb.github.io@main/Round_4/leaflet/leaflet.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/it-dainb/it-dainb.github.io@main/Round_4/leaflet/leaflet.rotatedMarker.js"></script>
+    <script src="https://cdn.jsdelivr.net/gh/it-dainb/it-dainb.github.io@main/Round_4/script.js"></script>
+    <!-- <script src="plugin.js"></script> -->
     `;
 
     // Append the div to the document body
